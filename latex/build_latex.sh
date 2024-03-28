96eb4236d39c0eabea6c3234cf54de1c41cbf2d7
Milton Pividori <miltondp@users.noreply.github.com>
2024-03-28 10:16:13 -0600

    fix build_latex.sh

--- latex/build_latex.sh
+++ latex/build_latex.sh
@@ -4,7 +4,10 @@ set -e
 latexmk -shell-escape
 
 # split
-pdftk manuscript.pdf cat 1-15 output manuscript-main_text.pdf
-pdftk manuscript.pdf cat 16-end output manuscript-supplementary_information.pdf
+pdftk manuscript.pdf cat 1-16 output manuscript-main_text.pdf
+pdftk manuscript.pdf cat 17-end output manuscript-supplementary_information.pdf
 
 # rm manuscript.pdf
+
+mv manuscript-main_text.pdf "Manuscript - Manubot AI Editor - v03.0.pdf"
+mv manuscript-supplementary_information.pdf "Supplementary Material - Manubot AI Editor - v03.0.pdf"
